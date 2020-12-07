80fa04262a8ddf656c24b832fd720d1362b13bf2
Fabian <fabianjuarezgaliote@gmail.com>
2020-12-07 01:06:33 -0600

    portfolio section v-1

--- assets/js/nav_animation.js
+++ assets/js/nav_animation.js
@@ -12,7 +12,7 @@ function classToggle(){
 
 	navClicked.addEventListener('click', (e)=>{
 
-		showNav.classList.toggle('hidden')
+		showNav.classList.toggle('hidden');
 		firtSpan[0].classList.toggle('closed');
 		getSpans[1].classList.toggle('nav-open');
 		getSpans[2].classList.toggle('nav-open-inverse');
@@ -22,22 +22,20 @@ function classToggle(){
 	for(let i = 0; i < navLinks.length; i++){
 		navLinks[i].addEventListener('click', (e)=>{
 
+			let buttonPortfolio;
 			let linkId = navLinks[i].textContent.toLowerCase();
 			let sectionId = document.getElementById(linkId);
 			if(sectionVisibleArea == undefined && linkId == 'home') {
 				sectionVisibleArea = 'home';
-				console.log(sectionVisibleArea);
 			}else if (sectionVisibleArea == undefined && linkId != 'home') {
 				sectionId.classList.replace('section-disactivate','section-active');
 				document.getElementById('home').classList.replace('section-active','section-disactivate');
 				sectionVisibleArea = linkId;
-				console.log(sectionVisibleArea);
 			}
 			if(sectionVisibleArea != linkId) {
 				sectionId.classList.replace('section-disactivate','section-active');
 				document.getElementById(sectionVisibleArea).classList.replace('section-active','section-disactivate');
 				sectionVisibleArea = linkId;
-				console.log('are a');
 			}
 
 
